0c7c8ab935530e0fc14e3e9c43ec771ad0174bc0
Andrew Zacharias <andyzack@gmail.com>
2020-02-17 00:20:59 +1100

    react hooks implementation - debug filter

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,21 +4,30 @@ import ProductCard from './ProductCard';
 
 function ProductList() {
   
+  // 1. Use the data state variable
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
 
+  // 2. Use the loading state variable
+  const [loading, setLoading] = useState(true);
+  const [hasError, setErrors] = useState(false);
+
+  // 3. Use an effect to fetch data using API Service
   useEffect(() => {
     APISERVICES()
     .then(setData)
+    .catch(err => setErrors(err))
   }, []);
 
   return(
+    
     <div className="product-container">
       <ul>
       {
+        !hasError? (
         data.map(PRODUCT => (
           <ProductCard key={PRODUCT.index} product={PRODUCT} />
         ))
+        ) : (<div>Error from API Service</div>)
       }
       </ul>
     </div>
