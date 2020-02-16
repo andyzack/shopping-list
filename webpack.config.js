b25bdf04c0117f55af425502dd9e0bf4483c8fa6
Andrew Zacharias <andyzack@gmail.com>
2020-02-16 22:25:51 +1100

    deploy scripts update

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,13 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    path: path.resolve(__dirname, "build/"),
+    publicPath: "/build/",
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/"
+    publicPath: "http://localhost:3000/build/"
   }
 };
