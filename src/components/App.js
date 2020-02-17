b5da0a185de35d80786a8f86c75510c4421739cd
Andrew Zacharias <andyzack@gmail.com>
2020-02-17 13:02:08 +1100

    react hooks implementation - add filter functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "../assets/styles/app.css";
 
-import ProductHeader from "./ProductHeader.js";
 import ProductList from "./ProductList";
 
 function App() {
   return(
     <div className="app">
-      <ProductHeader />
       <ProductList />
     </div>
   );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,33 +4,69 @@ import ProductCard from './ProductCard';
 
 function ProductList() {
   
-  // 1. Use the data state variable
+  // 1A) Use the data state variable
   const [data, setData] = useState([]);
 
-  // 2. Use the loading state variable
+  // 2B) Use the loading state variable
   const [loading, setLoading] = useState(true);
   const [hasError, setErrors] = useState(false);
 
-  // 3. Use an effect to fetch data using API Service
+  // 3B) Use an effect to fetch data using API Service
   useEffect(() => {
     APISERVICES()
-    .then(setData)
+    .then(data => {
+      setData(data)
+    })
     .catch(err => setErrors(err))
   }, []);
 
+  // 1B) Use the dress selected state variable
+  const [dressSize, setDressSize] = useState('');
+
+  // 2B) Use the data selected state variable
+  let [dataSelected, setDataSelected] = useState([]);
+
+  function handleChange(val, data) {
+    setDressSize(val)
+    dataSelected = data.filter(item => item.size.includes(val));
+    setDataSelected(dataSelected)
+  }
+
   return(
-    
+    <>
+    <div className="product-header">
+      <h1 className="product-title">Women’s tops {dressSize}</h1>
+      <select
+        className="product-filter"
+        value={dressSize}
+        onChange={(e) => {handleChange(e.target.value, data)}}
+      >
+        <option value="">Filter by size</option>
+        <option value="XL">Size XL</option>
+        <option value="L">Size L</option>
+        <option value="S">Size S</option>
+        <option value="XS">Size XS</option>
+      </select>
+    </div>
     <div className="product-container">
       <ul>
       {
         !hasError? (
-        data.map(PRODUCT => (
-          <ProductCard key={PRODUCT.index} product={PRODUCT} />
-        ))
+          (dressSize == '')? (
+            data.map(PRODUCT => (
+              <ProductCard key={PRODUCT.index} product={PRODUCT} />
+            ))
+          ) : (
+            dataSelected.map(PRODUCT => (
+              <ProductCard key={PRODUCT.index} product={PRODUCT} />
+            ))
+          )
+
         ) : (<div>Error from API Service</div>)
       }
       </ul>
     </div>
+    </>
   )
 }
 
