e68e67f7b3cf82fcba02b923084c69cd5f5c521a
Andrew Zacharias <andyzack@gmail.com>
2020-02-19 10:57:41 +1100

    Version 2: readable format

--- src/components/ProductOptions.js
+++ src/components/ProductOptions.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function ProductOptions(props) {
 
-  let dataSizes = props.product.map(item => item.size).flat()
-  .reduce((prev, next) => prev.includes(next) ? prev : [...prev, next], []);
+  let dataSizes = props.product
+                  .map(item => item.size).flat()
+                  .reduce((prev, next) => (
+                    prev.includes(next) ? prev : [...prev, next]
+                    ), []);
 
   return (
     <>
