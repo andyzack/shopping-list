7d8695598bb6d02292a3ea5fde0242420eef34c9
Andrew Zacharias <andyzack@gmail.com>
2020-02-24 14:25:27 +1100

    package update to support ES7 async-await

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ['@babel/polyfill', './src/index.js'],
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist/"),
